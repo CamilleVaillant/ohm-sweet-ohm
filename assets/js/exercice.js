ed8e90051e1393508ffd22a8720a699ebc3189af
Kev <kevin@MacBookPro.lan>
2024-11-08 09:38:09 +0100

    cards desktop ok

--- assets/js/exercice.js
+++ assets/js/exercice.js
@@ -1,8 +1,32 @@
 
+// Activation de la popup
+const stoneBtn = document.getElementById('stone_btn');
+const game = document.getElementById('breath_game');
+const gameClose = document.getElementById('gameClose');
+const popupClose = document.getElementsByClassName('popupClose');
+const sideMenu = document.getElementById('sideMenu');
+const validBtn = document.getElementById('setupValidBtn');
 
-    // Varaible pour l'initialisation du texte pour l'animation de respiration    
-    const textElement = document.querySelector('.text');
 
+// click bouton pour ouverture breath Game
+stoneBtn.addEventListener('click', function(event) {
+    event.preventDefault();
+    game.style.display = "block"
+});
+
+// fermeture du setup breath game avec submit ou bouton close
+function closeSetup(event) {
+    event.preventDefault();
+    sideMenu.classList.toggle('.collapsed');
+}
+for (let closePopupBtn of popupClose) {
+    closePopupBtn.onclick = closeSetup;
+}
+
+// popupClose.addEventListener('click', function(event) {
+//     event.preventDefault();
+//     sideMenu.classList.toggle('collapsed');
+// });
 
 // Validation formulaire pour valider les paramètres du breath game
 document.getElementById('setupBreath').addEventListener('submit', function(event) {
@@ -20,10 +44,30 @@ document.getElementById('setupBreath').addEventListener('submit', function(event
     }
 });
 
-// Gestion de la popup
 
-const popup = document.getElementById('popup');
+// script pour le text dynamique expirer inspirer
+// Varaible pour l'initialisation du texte pour l'animation de respiration    
+const textElement = document.querySelector('.text');
 
+    function updateBreathingText() {
+      // Vérifie si le texte actuel de `textElement` est "Inspirez"
+      // Si c'est le cas, il changera le texte en "Expirez"
+      // Sinon, il changera le texte en "Inspirez"
+    textElement.textContent = textElement.textContent === 'Inspirez' 
+      ? 'Expirez' // Si le texte est "Inspirez", il le remplace par "Expirez"
+      : 'Inspirez'; // Sinon, il remplace par "Inspirez"
+  }
+
+
+  // Fermeture du breath game 
+
+gameClose.addEventListener('click', function(event) {
+    event.preventDefault();
+    game.style.display = "none";
+});
+
+
+// PARTIE EXERCICES 
 
 
 // 3D rotation des card exercices
@@ -52,8 +96,8 @@ card.forEach(card => {
             const y = ev.clientY - boundingRef.top;
             const xPercentage = x / boundingRef.width;
             const yPercentage = y / boundingRef.height;
-            const xRotation = (xPercentage - 0.5) * 20;
-            const yRotation = (0.5 - yPercentage) * 20; // Inverstion pour avoir une homogénéité d'angle
+            const xRotation = (xPercentage - 0.5) * 10; // vérifier si degrés ok 
+            const yRotation = (0.5 - yPercentage) * 10; // Inversion pour avoir une homogénéité d'angle
 
             // inversion des axes pour la 3D. Inverser permet de conserver l'effet voulu
             // Ici, on permet l'appel via des variables CSS au hover
@@ -62,18 +106,3 @@ card.forEach(card => {
         }
     });
 });
-
-
-
-
-// script pour le text dinamique expiré inspiré
-
-//     function updateBreathingText() {
-//       // Vérifie si le texte actuel de `textElement` est "Inspirez"
-//       // Si c'est le cas, il changera le texte en "Expirez"
-//       // Sinon, il changera le texte en "Inspirez"
-//     textElement.textContent = textElement.textContent === 'Inspirez' 
-//       ? 'Expirez' // Si le texte est "Inspirez", il le remplace par "Expirez"
-//       : 'Inspirez'; // Sinon, il remplace par "Inspirez"
-//   }
-
